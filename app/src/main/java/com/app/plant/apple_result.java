e6b3e093038b333893302cdd497a6de2240f0d74
Umakant Mukhiya <umakant.1mu@gmail.com>
2020-10-26 23:48:05 +0530

    second commit

---
+++ app/src/main/java/com/app/plant/apple_result.java
@@ -0,0 +1,13 @@
+package com.app.plant;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class apple_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apple_result);
+    }
+}
